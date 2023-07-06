23cab5aa63a36ffe965b3815589213def65b0726
tjdgns5170 <129019459+tjdgns5170@users.noreply.github.com>
2023-07-06 17:26:14 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -54,53 +54,6 @@ $(document).ready(function(){
   }
   });
 
-  gsap.registerPlugin(ScrollTrigger);
-
-  let sections = gsap.utils.toArray(".sec");
-
-  let scrollTween = gsap.to(sections, {
-      xPercent: -100 * (sections.length - 1),
-      ease: "none",
-      scrollTrigger: {
-      trigger: ".horizontal-container",
-      pin: true,
-      scrub: 0.1,
-      end: "+=3000"
-      }
-  });
-
-  gsap.set(".box-1, .box-2", {y: 100});
-  // 스크롤마킹
-  // ScrollTrigger.defaults({markers: {startColor: "white", endColor: "white"}});
-
-  gsap.to(".box-1", {
-  y: -130,
-  duration: 2,
-  ease: "elastic",
-  scrollTrigger: {
-      trigger: ".box-1",
-      containerAnimation: scrollTween,
-      start: "left center",
-      toggleActions: "play none none reset",
-      id: "1",
-  }
-  });
-
-
-  gsap.to(".box-2", {
-  y: -120,
-  ease: "none",
-  scrollTrigger: {
-      trigger: ".box-2",
-      containerAnimation: scrollTween,
-      start: "center 80%",
-      end: "center 20%",
-      scrub: true,
-      id: "2"
-  }
-  });
-
-
   media();
   function media(){
     let windiwWidth = $(window).width();
@@ -112,6 +65,52 @@ $(document).ready(function(){
           delay: 1800,
           disableOnInteraction: false,
         },
+      });
+    }if(windiwWidth >= 1600){
+      gsap.registerPlugin(ScrollTrigger);
+
+      let sections = gsap.utils.toArray(".sec");
+    
+      let scrollTween = gsap.to(sections, {
+          xPercent: -100 * (sections.length - 1),
+          ease: "none",
+          scrollTrigger: {
+          trigger: ".horizontal-container",
+          pin: true,
+          scrub: 0.1,
+          end: "+=3000"
+          }
+      });
+    
+      gsap.set(".box-1, .box-2", {y: 100});
+      // 스크롤마킹
+      // ScrollTrigger.defaults({markers: {startColor: "white", endColor: "white"}});
+    
+      gsap.to(".box-1", {
+      y: -130,
+      duration: 2,
+      ease: "elastic",
+      scrollTrigger: {
+          trigger: ".box-1",
+          containerAnimation: scrollTween,
+          start: "left center",
+          toggleActions: "play none none reset",
+          id: "1",
+      }
+      });
+    
+    
+      gsap.to(".box-2", {
+      y: -120,
+      ease: "none",
+      scrollTrigger: {
+          trigger: ".box-2",
+          containerAnimation: scrollTween,
+          start: "center 80%",
+          end: "center 20%",
+          scrub: true,
+          id: "2"
+      }
       });
     }
   }
